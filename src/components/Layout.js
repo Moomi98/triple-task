8ab6c83251b92640d317be34776168a35198c56c
Moomi98 <kymkjh2002@gmail.com>
2022-06-18 10:29:18 +0900

    Feat - 전체적인 레이아웃 추가

---
+++ src/components/Layout.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const MainLayout = styled.div`
+  display: flex;
+  align-items: center;
+  justify-items: center;
+  min-width: 1200px;
+  height: 100%;
+  margin: 0 auto;
+  border: 1px solid red;
+`;
+
+const LogoLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  border: 1px solid blue;
+`;
+
+const InfoLayout = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Layout = () => {
+  return (
+    <MainLayout>
+      <LogoLayout></LogoLayout>
+      <InfoLayout></InfoLayout>
+    </MainLayout>
+  );
+};
+
+export default Layout;
