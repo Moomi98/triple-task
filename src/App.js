b202fd6d177796eb2b67550beb82b1915b68f432
Moomi98 <kymkjh2002@gmail.com>
2022-06-18 20:03:33 +0900

    feat : globalStyle 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Layout from "./components/Layout";
+import GlobalStyles from "./styles/globalStyles";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <Layout />
     </div>
   );
---
+++ src/styles/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    html,
+	body,
+	body > div,
+	.App,
+	main{
+		height : 100%;
+	}
+`;
+
+export default GlobalStyles;
